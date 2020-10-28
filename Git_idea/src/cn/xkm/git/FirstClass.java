ab3d4afcd5785da4534b198dc90d2aff55594a92
xiangkeming <751837630@qq.com>
2020-10-28 17:21:27 +0800

    idea第四次提交git

--- Git_idea/src/cn/xkm/git/FirstClass.java
+++ Git_idea/src/cn/xkm/git/FirstClass.java
@@ -3,6 +3,7 @@ package cn.xkm.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
-        System.out.println("第三次");
+        System.out.println("第四次");
+        int a = 1;
     }
 }
