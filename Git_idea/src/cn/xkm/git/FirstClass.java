cc36eba233b9ce70be976098d11503149b9f1c1b
xiangkeming <751837630@qq.com>
2020-10-28 17:19:18 +0800

    idea第二次提交git

---
+++ Git_idea/src/cn/xkm/git/FirstClass.java
@@ -0,0 +1,8 @@
+package cn.xkm.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("第二次");
+    }
+}
